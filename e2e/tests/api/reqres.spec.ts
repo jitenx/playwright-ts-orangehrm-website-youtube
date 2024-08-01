d13f6697bfd38ba2aa342e0a305bd5066627f2ee
jitenx <jituaec@yahoo.com>
2024-08-01 12:35:47 +0530

    new changes

--- e2e/tests/api/reqres.spec.ts
+++ e2e/tests/api/reqres.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { request } from "http";
 
 test("GET /api/users/2", async ({ request }) => {
   const response = await request.get("https:reqres.in/api/users/2");
@@ -6,3 +7,10 @@ test("GET /api/users/2", async ({ request }) => {
   const body = await response.json();
   expect(body.data.id).toBe(2);
 });
+
+test.only("Post Request",async ({request}) =>{
+  const response =await request.get("https:reqres.in/api/users/3");
+  expect(response.status()).toBe(200);
+  const body=await response.json();
+}
+)
